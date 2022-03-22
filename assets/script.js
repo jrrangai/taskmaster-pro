3537641b05ee020f25405f97bcb4c5e7bb5f17c3
Joshua Rangai <rangaij0116@hotmail.com>
2022-03-22 11:13:48 -0500

    edit task descriptions

--- assets/script.js
+++ assets/script.js
@@ -45,6 +45,50 @@ var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
 
+$(".list-group").on("click", "p", function() {
+  var text = $(this)
+    .text()
+    .trim();
+
+    var textInput = $("<textarea>")
+    .addClass("form-control")
+    .val(text);
+  
+  $(this).replaceWith(textInput);
+
+  textInput.trigger("focus");
+});
+
+$(".list-group").on("blur", "textarea", function() {
+  // get the textarea's current value/text
+  var text = $(this)
+    .val()
+    .trim();
+  // get the parents ul's id attribute
+  var status = $(this)
+    .closest(".list-group")
+    .attr("id")
+    .replace("list-", "");
+  // get the task's position in the list of other li elements
+  var index = $(this)
+    .closest(".list-group-item")
+    .index();
+
+  // return the text property of the object at the given index.
+  tasks[status][index].text = text;
+  // Updating this tasks object was necessary for localStorage, so we call saveTasks() immediately afterwards.
+  saveTasks();
+
+  // recreate p element
+  var taskP = $("<p>")
+    .addClass("m-1")
+    .text(text);
+
+  // replace textarea with the p element
+  $(this).replaceWith(taskP);
+
+});
+
 
 
 
